31a514a80533e3c852a5d83a1427444a1987da27
#云淡然 <cloudcome@163.com>
2018-08-18 15:50:05 +0800

    ~ unit test

--- test/test.index.js
+++ test/test.index.js
@@ -106,8 +106,7 @@ describe('测试文件', function () {
         })
         .action(function (options) {
             console.log('action init', options);
-        })
-        .command(commandGet());
+        });
 
     var options = {
         package: {
@@ -143,45 +142,3 @@ describe('测试文件', function () {
         ], options);
     });
 });
-
-
-function commandGet() {
-    var exports = {};
-    exports.command = 'get';
-    exports.describe = '根据配置获取远程信息';
-
-    exports.helper = true;
-    exports.options = {
-        domain: {
-            alias: 'd',
-            describe: '请求域名',
-            required: true
-        },
-        ssl: {
-            alias: ['s'],
-            type: 'boolean'
-        }
-    };
-
-    exports.usages = [
-        {
-            example: 'bin get'
-        },
-        {
-            example: 'bin get --domain',
-            describe: '根据域名获取远程地址的信息以展示给你看'
-        }
-    ];
-
-    exports.action = function (options) {
-        console.log('action get', options);
-    };
-
-    exports.error = function (key, option) {
-        console.log('出错啦，呵呵');
-        console.log('字段', key);
-        console.log('参数', option);
-    };
-
-    return exports;
-}
