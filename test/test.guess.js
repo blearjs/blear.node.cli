15dd42101e71b9e59ac80336e897e6415352722f
#云淡然 <cloudcome@163.com>
2018-08-28 14:35:53 +0800

    ~ guess print  banner test

--- test/test.guess.js
+++ test/test.guess.js
@@ -1,5 +1,5 @@
 /**
- * 文件描述
+ * test guess
  * @author ydr.me
  * @create 2018-08-20 14:51
  * @update 2018-08-20 14:51
@@ -25,13 +25,16 @@ describe('guess', function () {
     it('command', function (done) {
         var cli = new Cli();
         var fakeConsole = new FakeConsole();
+        var banner = Date.now() + '.' + Math.random();
 
         cli.$$injectConsole$$(fakeConsole);
         cli
+            .banner(banner)
             .command()
             .guess(function (command) {
                 expect(this).toBe(cli);
                 expect(command).toBe('abc');
+                expect(fakeConsole.get()).toEqual(banner + '\n');
                 done();
             })
             .parse(argv('abc'), options);
